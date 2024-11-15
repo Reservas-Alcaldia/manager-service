9c349ec912bbfab028f05232c384d98b8054f183
Cardona-JE <juancardonaa0421@gmail.com>
2024-11-15 16:32:37 -0500

    Gestor: Add endpoints to approve, disapprove and get all requests

--- src/Modules/Gestor/Module/gestor.module.ts
+++ src/Modules/Gestor/Module/gestor.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GestorController } from '../controllers/gestor.controller';
 import { GestorService } from '../Services/gestor.service';
 import { Solicitud } from 'src/Entities/solicitud.entity'; 
+import { Sala } from 'src/Entities/sala.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Solicitud])], 
+  imports: [TypeOrmModule.forFeature([Solicitud, Sala])], 
   controllers: [GestorController],
   providers: [GestorService],
 })
--- src/Modules/Gestor/controllers/gestor.controller.ts
+++ src/Modules/Gestor/controllers/gestor.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Put } from '@nestjs/common';
 import { GestorService } from '../Services/gestor.service';
 
 @Controller('solicitudes')
 export class GestorController {
   constructor(private readonly gestorService: GestorService) {}
+
+  @Get('/all')
+  async getAllSolicitudes() {
+    const solicitudes = await this.gestorService.getAllSolicitudes();
+    return this.createResponse(solicitudes);
+  }
 
   @Get('/approved')
   async getSolicitudesAprobadas() {
@@ -22,6 +28,34 @@ export class GestorController {
     const solicitudes = await this.gestorService.getSolicitudesEnEspera();
     return this.createResponse(solicitudes);
   }
+
+  @Put('/approve/:id')
+  async aprobarSolicitud(
+    @Param('id') id: number,
+    @Body() body: { salaId: number; horaInicio?: string; horaFin?: string },
+  ) {
+    const solicitudAprobada = await this.gestorService.aprobarSolicitud(
+      id,
+      body.salaId,
+      body.horaInicio,
+      body.horaFin,
+    );
+    return {
+      status: HttpStatus.OK,
+      message: `La solicitud con ID: ${id} ha sido aprobada.`,
+    };
+  }
+
+  @Put('/disapprove/:id')
+  async rechazarSolicitud(
+    @Param('id') id: number,
+  ) {
+    const solicitudAprobada = await this.gestorService.rechazarSolicitud(id);
+    return {
+      status: HttpStatus.BAD_REQUEST,
+      message: `La solicitud con ID: ${id} ha sido rechazada.`,
+    };
+  }
   
   private createResponse(data: any) {
     return {
@@ -30,4 +64,5 @@ export class GestorController {
       data: data,
     };
   }
+
 }
