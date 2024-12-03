751c899e51f3ad336cc075627040152da8ec05df
juansecod <juan.sebastianri28@gmail.com>
2024-12-03 13:33:52 -0500

    Fix database client to mssql

--- src/Common/Database/Database.module.ts
+++ src/Common/Database/Database.module.ts
@@ -5,13 +5,14 @@ import { Secretaria } from "src/Entities/secretaria.entity";
 import { Solicitud } from "src/Entities/solicitud.entity";
 import { Connection } from "typeorm";
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 dotenv.config();
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
-            type: process.env.DB_TYPE as 'mysql',
+            type: process.env.DB_TYPE as  "mssql",
             host: process.env.DB_HOST,
             port: parseInt(process.env.DB_PORT, 10),
             username: process.env.DB_USERNAME,
