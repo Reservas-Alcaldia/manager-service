deecddf4f7af83757eeaf0313d99ac2a783877e0
juansecod <juan.sebastianri28@gmail.com>
2024-12-03 17:07:24 -0500

    Fix port and cli dependecy

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new CustomValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
